4376b221c912c6ba552535991901d8a619e75d16
X0AM <watsonjude8@gmail.com>
2022-03-26 18:37:36 +0000

    cool red thingy

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Platformer Game")
 clock = pygame.time.Clock()
+test = pygame.Surface((300, 200))
+test.fill('Red')
 
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       exit()
+  screen.blit(test, (0, 0))
   pygame.display.update()
   clock.tick(60)
-
-  print("hi")
