dcc7f6ab8923cf8fbb2d50ebd27c530e54e72a98
X0AM <watsonjude8@gmail.com>
2022-03-27 14:04:11 +0100

    added title and "enemy". Enemy does not work yet.

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ pygame.display.set_caption("Platformer Game")
 clock = pygame.time.Clock()
 bg = pygame.Surface((800, 400))
 ground = pygame.Surface((800, 200))
+font = pygame.font.Font('font/nova.ttf', 30)
+enemy = pygame.Surface((50, 50))
+enemyOriginalX = 600
+enemyOriginalY = 250
+enemySpeed = 4
+
 bg.fill('blue')
 ground.fill('green')
+title = font.render('Platformer Game', False, 'yellow')
+enemy.fill('purple')
+
 
 while True:
   for event in pygame.event.get():
@@ -17,5 +26,10 @@ while True:
       exit()
   screen.blit(bg, (0, 0))
   screen.blit(ground, (0, 300))
+  screen.blit(title, (275, 50))
+  screen.blit(enemy,(enemyOriginalX, enemyOriginalY))
   pygame.display.update()
+  enemyOriginalX -= enemySpeed
+  if enemyOriginalX < -100:
+    enemyOriginalX = 800
   clock.tick(60)
