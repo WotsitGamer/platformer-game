98637aa9840e3d0fe242f05277ab294757490dc3
X0AM <watsonjude8@gmail.com>
2022-03-27 13:36:33 +0100

    added basic ground sprite and sky sprite

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Platformer Game")
 clock = pygame.time.Clock()
-test = pygame.Surface((300, 200))
-test.fill('Red')
+bg = pygame.Surface((800, 400))
+ground = pygame.Surface((800, 200))
+bg.fill('blue')
+ground.fill('green')
 
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       exit()
-  screen.blit(test, (0, 0))
+  screen.blit(bg, (0, 0))
+  screen.blit(ground, (0, 300))
   pygame.display.update()
   clock.tick(60)
